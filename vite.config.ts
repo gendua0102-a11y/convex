25992beb254433b7eea88eb7f82c09c473d81c48
gendua0102-a11y <gendua0102@gmail.com>
2026-05-01 19:45:25 +0000

    Update package.json and vite config with correct plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react'
+import react from '@vitejs/plugin-react' // Note the 'plugin-' part here
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Since your repo is named "convex", use that here
-  base: '/convex/',
+  base: '/convex/', 
 })
